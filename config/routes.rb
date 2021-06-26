918e32cd565e8d09f0fabe1b11bb91d9e0f70178
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-06-26 15:36:15 -0700

    commit tree 530ce7bf71c766671c14400b6c6377a2dce4c015

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :new, :create]
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  put 'posts/:id', to: 'posts#update'
 end
